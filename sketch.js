13fd85beab52995f006aa6d5a8ac781d8cdebf6e
Elpapi2727 <85907374+Elpapi2727@users.noreply.github.com>
2021-07-27 18:13:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,14 +18,23 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	roofObject=new roof(400,250,230,20);
+	roofObject=new roof(400,250,300,20);
 	bob1 = new bob(320,575,40)
 	bob2 = new bob(360,575,40)
+	bob3 = new bob(400,575,40)
+	bob4 = new bob(440,575,40)
+	bob5 = new bob(480,575,40)
+	bob6 = new bob(520,575,40)
+	bob7 = new bob(-42,300,40)
 	
 	
 	rope1=new rope(bob1.body,roofObject.body,-80, 0)
 	rope2=new rope(bob2.body,roofObject.body,-40, 0)
-	
+	rope3=new rope(bob3.body,roofObject.body,0, 0)
+	rope4=new rope(bob4.body,roofObject.body,40, 0)
+	rope5=new rope(bob5.body,roofObject.body,80, 0)
+	rope6=new rope(bob6.body,roofObject.body,120, 0)
+	rope7=new rope(bob7.body,roofObject.body,-120, 0)
 	
 	Engine.run(engine);
 	
@@ -40,11 +49,19 @@ function draw() {
 
   rope1.display();
   rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+  rope6.display();
+  rope7.display();
 
   bob1.display();
   bob2.display();
-  
-  
+  bob3.display();
+  bob4.display();
+  bob5.display();
+  bob6.display();
+  bob7.display();
  
 }
 
